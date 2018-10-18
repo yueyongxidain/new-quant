72cbf667403f77cc06b1eb65e963ebb828b1a6b7
liuwenzhu <547485261@qq.com>
2018-10-18 13:44:02 +0800

    add
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/routes/Exception/404.js
+++ src/routes/Exception/404.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'dva/router';
-import {Exception} from 'quant-ui';
+import { Exception } from 'quant-ui';
 
 export default () => (
-  <Exception type="404" style={{ minHeight: 500, height: '80%' }} linkElement={Link} />
+    <Exception type="404" style={{ minHeight: 500, height: '80%' }} linkElement={Link} />
 );
