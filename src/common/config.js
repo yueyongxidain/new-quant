687116b70d9170cd09c4c44f3cca0dc4974756cf
liuwenzhu <547485261@qq.com>
2018-10-22 14:22:05 +0800

    add
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/common/config.js
+++ src/common/config.js
@@ -2,7 +2,7 @@
  * @Author: 刘文柱 
  * @Date: 2018-10-18 10:10:38 
  * @Last Modified by: 刘文柱
- * @Last Modified time: 2018-10-22 14:20:21
+ * @Last Modified time: 2018-10-22 14:21:37
  */
 import { language, Icon } from "quant-ui";
 import React from 'react';
@@ -23,7 +23,7 @@ let zhConfig = {
     "Help": flowImg,                        //帮助文档  bas64图片 或者 url
     "privacy": "当前版本:0.125",              //关于
     "flowImg": flowImg,                      //首页流程图
-    "isTop": "{{isTop}}",                               //导航栏是否在顶部
+    "isTop": {{isTop}},                               //导航栏是否在顶部
     "FOOTER": <span>Copyright <Icon type="copyright" /> 2018 量投极速柜台交易系统</span>  //页脚
 }
 
@@ -39,7 +39,7 @@ let enConfig = {
     "Help": flowImg,                        //帮助文档  bas64图片 或者 url
     "privacy": "当前版本:0.125",              //关于
     "flowImg": flowImg,                      //首页流程图
-    "isTop": "{{description}}", 
+    "isTop": {{isTop}},   
     "FOOTER": <span>Copyright <Icon type="copyright" /> 2018 QuantDo Plantform</span>  //页脚
 }
 
